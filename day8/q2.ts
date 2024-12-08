dd7326c88e881e949e88fea4712927b3759e8d33
chris <chris.nelson@campspot.com>
2024-12-08 12:04:06 -0700

    d8p2

--- day8/q2.ts
+++ day8/q2.ts
@@ -1,48 +1,74 @@
 import fs from "fs";
+import cartesian from "fast-cartesian";
+import { createBuilderStatusReporter } from "typescript";
 
-let ops = ["*", "+", "|"];
-let testValTot = 0;
+const nodes = new Map<string, number[][]>();
+const antiNodes: string[] = [] 
 
-const map = fs
-  .readFileSync("day7/input.txt")
+fs.readFileSync("day8/input.txt")
   .toString()
   .split("\n")
-  .forEach((l) => {
-    const tot = parseInt(l.split(": ")[0]);
-    const terms = l
-      .split(": ")[1]
-      .split(" ")
-      .map((n) => +n);
+  .forEach((l, x) => {
+    antiNodes.push(Array(l.length).fill(".").join(""));
 
-    if (dfsCheck(terms, tot, 0, terms[0])) {
-      testValTot += tot;
-    }
+    [...l].forEach((c, y) => {
+      if (c != ".") {
+        nodes.has(c) ? nodes.get(c)!.push([x, y]) : nodes.set(c, [[x, y]]);
+      }
+    });
   });
 
-console.log(testValTot);
-
-function dfsCheck(terms: number[], target: number, index: number, currentTotal: number): boolean {
-  if(currentTotal > target) return false
-  if (index === terms.length - 1) {
-    return currentTotal === target;
-  }
-
-  for (const op of ops) {
-    const nextTerm = terms[index + 1];
-    let newTotal = currentTotal;
-
-    if (op === "*") {
-      newTotal *= nextTerm;
-    } else if (op === "+") {
-      newTotal += nextTerm;
-    } else if (op === "|") {
-      newTotal = +`${newTotal}${nextTerm}`;
-    }
-
-    if (dfsCheck(terms, target, index + 1, newTotal)) {
-      return true;
-    }
-  }
-
-  return false; 
+for (const node of nodes.keys()) {
+  findAntinodes(nodes.get(node)!);
 }
+
+function findAntinodes(points: number[][]) {
+  if (points.length > 1) {
+    points
+      .flatMap((v, i) => points.slice(i + 1).map((w) => [v, w]))
+      .forEach((nodePair) => {
+        const x1 = nodePair[0][0]
+        const x2 = nodePair[1][0]
+        const y1 = nodePair[0][1]
+        const y2 = nodePair[1][1]
+
+        const slope = (x1 === x2) ? null : (y2- y1) / (x2 - x1);
+        const dX = Math.abs(x1 - x2)
+        const dY = Math.abs(y1 - y2)
+
+        let curX = x1
+        let curY = y1
+        do {
+          antiNodes[curX] = antiNodes[curX].substring(0, curY) + '#' + antiNodes[curX].substring(curY + 1);
+          if (slope && slope >0 ) {
+            curX += dX
+            curY += dY
+          } else {
+            curX -= dX
+            curY += dY
+          }
+
+        } while (curX >= 0 && curX < antiNodes.length && curY >= 0 && curY < antiNodes[0].length)
+
+        curX = x1
+        curY = y1
+        do {
+          antiNodes[curX] = antiNodes[curX].substring(0, curY) + '#' + antiNodes[curX].substring(curY + 1);
+          if (slope && slope >0 ) {
+            curX -= dX
+            curY -= dY
+          } else {
+            curX += dX
+            curY -= dY
+          }
+
+        } while (curX >= 0 && curX < antiNodes.length && curY >= 0 && curY < antiNodes[0].length)
+      });
+  }
+}
+
+antiNodes.forEach(a=>console.log(a))
+
+console.log(antiNodes.reduce((a,c) => {
+  return a + [...c].filter(v=>v=="#").length
+},0))
