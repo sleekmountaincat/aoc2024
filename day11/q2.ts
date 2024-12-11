ca4dfdbb62216b77fd3bd764d751a572956091c7
chris <chris.nelson@campspot.com>
2024-12-11 11:33:23 -0700

    p2!!!!

--- day11/q2.ts
+++ day11/q2.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
-import moize from "moize";
+import memoize from 'memoize';
 
 let sum = 0;
+const lengths = new Map<string, number>()
 
 let blinkStone = (num: number): number[] => {
   if (num === 0) return [1, -1];
@@ -12,23 +13,28 @@ let blinkStone = (num: number): number[] => {
     ];
   else return [num * 2024, -1];
 };
-const blinkStoneM = moize(blinkStone);
+
+blinkStone = memoize(blinkStone);
 
 let stoneLength = (num: number, blinkCnt: number): number => {
-  const [l, r] = blinkStoneM(num);
+  const [l, r] = blinkStone(num);
   if (blinkCnt == 1) return r > -1 ? 2 : 1;
 
-  return r > -1
-    ? stoneLength(r, blinkCnt - 1) + stoneLength(l, blinkCnt - 1)
-    : stoneLength(l, blinkCnt - 1);
+  let length = stoneLength(l, blinkCnt - 1);
+  if (r > -1) length += stoneLength(r, blinkCnt - 1)
+  
+  return length
 };
-const stoneLengthM = moize(stoneLength);
+
+stoneLength = memoize(stoneLength, {
+  cacheKey: (arguments_: number[]) => arguments_.join(',')
+});
 
 fs.readFileSync("day11/input.txt")
   .toString()
   .split("\n")
   .forEach(
-    (l) => (sum += l.split(" ").reduce((a, c) => a + stoneLengthM(+c, 75), 0))
+    (l) => (sum += l.split(" ").reduce((a, c) => a + stoneLength(+c, 75), 0))
   );
 
 console.log(sum);
