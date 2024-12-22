90b90e1904b311140e390a5321ec3aa420a10953
chris <chris.nelson@campspot.com>
2024-12-21 23:49:11 -0700

    hmmm

---
+++ day22/q1.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+
+const steps = 2000;
+let sum = 0;
+
+fs.readFileSync("day22/input.txt")
+  .toString()
+  .split("\n")
+  .forEach((n) => {
+    let num = +n;
+    for (let x = 0; x < steps; x++) {
+      num = getSecret(num);
+    }
+    sum += num;
+  });
+
+console.log(sum);
+
+function getSecret(n: number): number {
+  return (
+    ((((n ^ (n * 64)) % 16777216 ^ Math.floor(n / 32)) % 16777216 ^
+      ((((n ^ (n * 64)) % 16777216 ^ Math.floor(n / 32)) % 16777216) *
+        2048)) >>>
+      0) %
+    16777216
+  );
+}
