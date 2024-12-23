c46e6073e6632dac489dab93f70e826f521357e3
chris <chris.nelson@campspot.com>
2024-12-22 23:26:03 -0700

    d23

---
+++ day23/q1.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+const puters = new Map<string, string[]>();
+
+fs.readFileSync("day23/input.txt")
+  .toString()
+  .split("\n")
+  .forEach((l) => {
+    const [c1, c2] = l.split("-");
+
+    puters.has(c1) ? puters.get(c1)!.push(c2) : puters.set(c1, [c2]);
+    puters.has(c2) ? puters.get(c2)!.push(c1) : puters.set(c2, [c1]);
+  });
+
+const cx = new Set<string>();
+for (const [p, n] of puters) {
+  if (p.startsWith("t")) {
+    n.flatMap((v, i) => n.slice(i + 1).map((w) => v + "-" + w)).forEach(
+      (pair) => {
+        const [p1, p2] = pair.split("-");
+        if (puters.get(p1)!.includes(p2)) {
+          cx.add([p, p1, p2].sort().join("-"));
+        }
+      }
+    );
+  }
+}
+
+console.log(cx.size);
